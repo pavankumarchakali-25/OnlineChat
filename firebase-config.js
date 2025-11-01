039264edbe10258e2bd894b95e59e59913674dec
pavankumarchakali-25 <c.pavan9949@gmail.com>
2025-11-01 18:30:51 +0530

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,27 @@
-// Import Firebase SDKs
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-analytics.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+// ✅ Firebase Compat setup — no imports needed
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyDQKaQQBwrnJ71dOLFMj_uqMqD2vlLigII",
-  authDomain: "chat-application-online.firebaseapp.com",
-  projectId: "chat-application-online",
-  storageBucket: "chat-application-online.firebasestorage.app",
-  messagingSenderId: "796740928101",
-  appId: "1:796740928101:web:2cda1edc963935902abc8c",
-  measurementId: "G-W2WDVZH8LQ"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDQKaQQBwrnJ71dOLFMj_uqMqD2vlLigII",
+    authDomain: "chat-application-online.firebaseapp.com",
+    projectId: "chat-application-online",
+    storageBucket: "chat-application-online.firebasestorage.app",
+    messagingSenderId: "796740928101",
+    appId: "1:796740928101:web:2cda1edc963935902abc8c",
+    measurementId: "G-W2WDVZH8LQ"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-// Export services
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+firebase.initializeApp(firebaseConfig);
+
+// global references
+const auth = firebase.auth();
+const db = firebase.firestore();
+const rtdb = firebase.database();
+const storage = firebase.storage();
