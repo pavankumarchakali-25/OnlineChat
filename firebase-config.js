44db15450839b657df76f4e0c5e00803bcf39ed5
pavankumarchakali-25 <c.pavan9949@gmail.com>
2025-11-01 18:34:31 +0530

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,15 @@
+// firebase-config.js (Compat version)
+const firebaseConfig = {
+  apiKey: "AIzaSyDQKaQQBwrnJ71dOLFMj_uqMqD2vlLigII",
+  authDomain: "chat-application-online.firebaseapp.com",
+  projectId: "chat-application-online",
+  storageBucket: "chat-application-online.appspot.com",  // ✅ fixed
+  messagingSenderId: "796740928101",
+  appId: "1:796740928101:web:2cda1edc963935902abc8c",
+  measurementId: "G-W2WDVZH8LQ"
+};
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDQKaQQBwrnJ71dOLFMj_uqMqD2vlLigII",
-    authDomain: "chat-application-online.firebaseapp.com",
-    projectId: "chat-application-online",
-    storageBucket: "chat-application-online.firebasestorage.app",
-    messagingSenderId: "796740928101",
-    appId: "1:796740928101:web:2cda1edc963935902abc8c",
-    measurementId: "G-W2WDVZH8LQ"
-  };
-
-
-
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // global references
@@ -18,3 +17,5 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 const rtdb = firebase.database();
 const storage = firebase.storage();
+
+console.log("🔥 Firebase initialized successfully:", firebase.app().name);
